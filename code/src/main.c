3bdb4e7fd65ddb16c44b7b31e3add5713f71980d
LhgS <lucas.henrique.gois.santos@gmail.com>
2025-05-04 11:55:40 -0300

    alterando setup

--- code/src/main.c
+++ code/src/main.c
@@ -1,5 +1,6 @@
 #include "settings.h"
 
+
 void setup(ecs_world_t* world);
 void game_loop(void);
 void destroy_game(ecs_world_t* world);
@@ -22,15 +23,15 @@ void setup(ecs_world_t* world){
     const int screenHeight = WINDOW_HEIGHT;
 
     InitWindow(screenWidth, screenHeight, "Pokeclone");
-}
 
-void game_loop(void) {
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
+}
+
+void game_loop(void) {
     // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
+    while (!WindowShouldClose()) {  // Detect window close button or ESC key
         // Update
         //----------------------------------------------------------------------------------
         // TODO: Update your variables here
